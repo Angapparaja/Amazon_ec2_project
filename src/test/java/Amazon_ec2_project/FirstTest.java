b8de98184be072f794fd37700728f5802bf594bc
Angapparaja <88724733+Angapparaja@users.noreply.github.com>
2022-06-06 13:16:26 +0530

    code updatedd

--- src/test/java/Amazon_ec2_project/FirstTest.java
+++ src/test/java/Amazon_ec2_project/FirstTest.java
@@ -13,7 +13,7 @@ public class FirstTest {
 	@Test
 	public void OpenBrowser() {
 		WebDriver driver;
-		System.setProperty("webDriver.chrome.driver"," /usr/bin/google-chrome");
+		System.setProperty("webDriver.chrome.driver","/usr/bin/chromedriver");
 		ChromeOptions option =new ChromeOptions();
 		option.addArguments("headless");
 		driver=new ChromeDriver();
